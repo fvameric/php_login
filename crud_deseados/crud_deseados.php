5df181361b9154eea1d941aaec3ed09929b4f445
fvameric <fvameric@gmail.com>
2021-11-30 17:16:56 +0100

    Branch main restore

--- crud_deseados/crud_deseados.php
+++ crud_deseados/crud_deseados.php
@@ -1,65 +1,60 @@
 <?php
-include_once('/conexion/db.php');
-include_once('/clases/deseados.php');
+    class CrudDeseados {
+        public function __construc(){}
 
-class CrudDeseados
-{
-    private $db;
-    private $listaDeseados = [];
+        public function mostrar() {
+            include 'db.php';
 
-    function __construct()
-    {
-        $this->db = new db();
-        $this->cargarDeseados();
-    }
+            $listaDeseados=[];
 
-    public function cargarDeseados()
-    {
-        $sql = "SELECT * FROM `deseados` WHERE 1";
-        $consulta = mysqli_query($this->db->obtenerConexion(), $sql);
+            $sql="SELECT * FROM `deseados` WHERE 1";
+            $consulta = mysqli_query($con,$sql);
 
-        while ($fila = $consulta->fetch_assoc()) {
-            $newDeseado = new Deseados();
-            $newDeseado->setId($fila['id']);
-            $newDeseado->setUserId($fila['user_id']);
-            $newDeseado->setPlantaId($fila['planta_id']);
+            while($fila=$consulta->fetch_assoc()) {
+                $newDeseado = new Deseados();
+                $newDeseado->setId($fila['id']);
+                $newDeseado->setUserId($fila['user_id']);
+                $newDeseado->setPlantaId($fila['planta_id']);
 
-            $this->listaDeseados[] = $newDeseado;
+                $listaDeseados[] = $newDeseado;
+            }
+            return $listaDeseados;
         }
-    }
 
-    public function obtenerListaDeseados()
-    {
-        return $this->listaDeseados;
-    }
+        public function agregarDeseado($user_id, $planta_id) {
+            include 'db.php';
 
-    public function obtenerDeseadoPorId($id_planta, $id_user)
-    {
-        foreach ($this->listaDeseados as $deseado) {
-            if ($id_planta == $deseado->getPlantaId() && $id_user == $deseado->getUserId()) {
-                return $deseado;
+            $sql = "INSERT INTO `deseados` (`id` ,`user_id` ,`planta_id`)VALUES (NULL , '".$user_id."', '".$planta_id."')";
+            return $consulta = mysqli_query($con,$sql);
+        }
+
+        public function eliminarDeseado($id_deseado) {
+            include 'db.php';
+            
+            $sqlDelete="DELETE FROM `deseados` WHERE id=".$id_deseado;
+            $consulta = mysqli_query($con,$sqlDelete);
+        }
+
+        public function modificarDeseados($deseadoModif, $id_deseado) {
+            include 'db.php';
+            $sqlUpdate="UPDATE `deseados` SET id=".$id_deseado.", user_id=".$deseadoModif->getUserId().", planta_id=".$deseadoModif->getPlantaId().")";
+            $consultaUpdate = mysqli_query($con, $sqlUpdate);
+        }
+
+        public function obtenerDeseado($id_planta, $id_user) {
+            include 'db.php';
+
+            $sqlId="SELECT * FROM `deseados` WHERE user_id = ".$id_user." AND planta_id=".$id_planta;
+            $consultaDeseados = mysqli_query($con, $sqlId);
+            $fila = $consultaDeseados->fetch_assoc();
+            
+            $idDeseado = $fila['id'];
+            
+            if (!empty($fila)) {
+                return $idDeseado;
+            } else {
+                return null;
             }
         }
-        return null;
     }
-
-    public function agregarDeseado($user_id, $planta_id)
-    {
-        $sql = "INSERT INTO `deseados` (`id` ,`user_id` ,`planta_id`)VALUES (NULL , '" . $user_id . "', '" . $planta_id . "')";
-        return $consulta = mysqli_query($this->db->obtenerConexion(), $sql);
-    }
-
-    public function eliminarDeseado($id_deseado)
-    {
-        $sqlDelete = "DELETE FROM `deseados` WHERE id=" . $id_deseado;
-        $consulta = mysqli_query($this->db->obtenerConexion(), $sqlDelete);
-    }
-
-    public function modificarDeseados($deseadoModif, $id_deseado)
-    {
-        $sqlUpdate = "UPDATE `deseados` SET id=" . $id_deseado . ", user_id=" . $deseadoModif->getUserId() . ", planta_id=" . $deseadoModif->getPlantaId() . ")";
-        $consultaUpdate = mysqli_query($this->db->obtenerConexion(), $sqlUpdate);
-    }
-}
-
 ?>
